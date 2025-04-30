488b9850d43cee9658f15836707b6760ee8ec1f6
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2025-04-30 23:42:00 +0530

    timeout increase

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,10 @@ import "../models/order.model";
 
 export async function connectDB() {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI,{
+      serverSelectionTimeoutMS: 30000,  
+      connectTimeoutMS: 30000, 
+    });
     return connection;
   } catch (error) {
     console.error(error.message);
